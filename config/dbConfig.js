67f096211905e169b1e8dd5cd82c1d00271bdf2c
RameshTanugula <gnitramesh@gmail.com>
2022-11-25 12:08:20 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,9 @@
 module.exports = {
-    host: "localhost",
-    port: 3306,
-    database: "morning_tax",
-    user: "root", 
-    password: "Admin@123"
-  };
+  host: "database-1.ccjosn0u7g6u.ap-south-1.rds.amazonaws.com",
+  port: 3306,
+  database: "morning_tax",
+  user: "admin", 
+  //admin 
+  password: "Pa33word.1", // Morningtax//Newdatabase
+  
+};
